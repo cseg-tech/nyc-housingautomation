cd8b7d700f7fb9dff435aff069221c7525db1bcd
RamiMatar <36045320+RamiMatar@users.noreply.github.com>
2019-11-06 22:13:34 -0500

    Unit Tests now working

--- tests.py
+++ tests.py
@@ -1,8 +1,11 @@
 import unittest
- 
+import json
+import random, string
 from app import app 
 from app.modules import Credential
 from app.modules import MongoHelper
+from app.modules import NYCDBWrapper
+from flask import Flask, current_app
 
 db, col = MongoHelper.init_Mongo()
 
@@ -15,6 +18,7 @@ class AutomationTests(unittest.TestCase):
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
         self.app = app.test_client()
+        self.APP = Flask(__name__)
  
     # executed after each test
     def tearDown(self):
@@ -31,34 +35,47 @@ class AutomationTests(unittest.TestCase):
     def test_login(self):
         sample_login = Credential.getSampleLogin()
         sample_pass = Credential.getSamplePassword()
-
-        # Check if account exists, asset
+        with self.APP.app_context():
+            result = MongoHelper.DB_login_user(db, col, sample_login, sample_pass, 0).get_json()
+            self.assertEqual(result["status"], '0')
+            result = MongoHelper.DB_login_user(db, col, "notanemail", sample_pass, 0).get_json()
+            self.assertEqual(result["status"], '2')
+            result = MongoHelper.DB_login_user(db, col, sample_login, sample_pass + "wrongpass", 0).get_json()
+            self.assertEqual(result["status"], '1')
         return True
 
     # Begin test to check signup functionality
     def test_signup(self):
         sample_login = "nonexistentemail@test.com"
         sample_pass = "thisemaildoesntexist"
-
-        # Rami to work with Chris to delete an email from the database
+        existing_login = Credential.getSampleLogin()
+        identifier = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+        with self.APP.app_context():
+            result = MongoHelper.DB_register_user(db, col, identifier, sample_login, sample_pass, "70 Morningside Drive", 1019610057, 0).get_json()
+            self.assertEqual(result["status"], '0')
+            MongoHelper.DB_remove_user(col, sample_login)
+            result = MongoHelper.DB_register_user(db, col, identifier, existing_login, sample_pass, "70 Morningside Drive", 1019610057, 0).get_json()
+            self.assertEqual(result["status"], '1')
         return True
 
-    # Begin test to check find complaonts for BBL functionality
+    # # Begin test to check find complaonts for BBL functionality
     def test_find_all_complaints(self):
         testBBL = 1019610057
-
+        complaints = NYCDBWrapper.findAllComplaints(testBBL)
+        self.assertIsNotNone(complaints[0])
+        self.assertIsNotNone(complaints[1])
         # Check if closed and open complaints are both not null
-
         return True
 
-    # Begin test to check get BBL from address functionality
+    # # Begin test to check get BBL from address functionality
     def test_get_bbl(self):
 
         # Call NYCDBWrapper.getBBL(housenum, street, borough) and check that the return value is not null for the below inputs
         house_num = "70"
         street = "Morningside Drive"
         borough = "manhattan"
-
+        bbl = NYCDBWrapper.getBBL(house_num, street, borough)
+        self.assertIsNotNone(bbl)
         return True
 
     # Begin test to check email functionality
